6babe8d895c2bea33c23327c4324ae08851f3c93
Falkenmarlin <falkenmarlin@gmail.com>
2025-01-05 21:12:48 +0100

    Update test-year.php

--- test-year.php
+++ test-year.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error - 404</title>
+    <title>Test year</title>
     <link rel="icon" href="nmb.jpg" type="image/jpg">
     <link rel="stylesheet" href="stylesheet.css">
 </head>
