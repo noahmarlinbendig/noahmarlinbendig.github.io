0e134d7f9cd67016cba103f61dc21cb700c92256
Falkenmarlin <falkenmarlin@gmail.com>
2025-01-05 21:11:50 +0100

    Create test-year.php

---
+++ test-year.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error - 404</title>
+    <link rel="icon" href="nmb.jpg" type="image/jpg">
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <main>
+      <h2>Test</h2>
+      <p>Testing if the current year can be displayed with php.</p>
+      <?php echo date("Y"); ?>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Noah Marlin Bendig. Alle Rechte vorbehalten.</p>
+    </footer>    
+</body>
